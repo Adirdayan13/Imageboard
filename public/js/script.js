38298e5050e66252e45e91c720fcffc49bc0a849
Adir Dayan <adirdayan@gmail.com>
2020-01-20 15:02:48 +0100

    create branch

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+(function() {
+    window.app = new Vue({
+        el: "#main",
+        data: {
+            heading: "Welcome",
+            greetee: "Kitty",
+            className: "pretty",
+            url: "https://www.spiced.academy",
+            candy: ["malteasers", "kinder"]
+        }
+    });
+})();
